63304704644979c60232dcdd2d022c4f7c67cda2
unknown <emuto@unsa.edu.pe>
2024-09-18 10:30:46 -0500

    create header

--- Proyecto/html/HTMLGenerator.java
+++ Proyecto/html/HTMLGenerator.java
@@ -1,10 +1,15 @@
 package html;
 public class HTMLGenerator {
 	public static void main(String[]args) {
-		System.out.print(createTitle("Pato"));
+		System.out.print(createHeader("Pato"));
 	}
 	public static String createTitle(String title) {
 		String html="<head>\n<title>"+title+"</title>\n</head>";
 		return html;
 	}
+	public static String createHeader(String header) {
+		String html="<head>\n<header>"+header+"</header>\n</head>";
+		return html;
+	}
+	
 }
