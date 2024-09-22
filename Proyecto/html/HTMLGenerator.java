92affa2959933d336472cd0d4ff283aea3f021d0
Kazel 10 <echoquequi@unsa.edu.pe>
2024-09-22 11:14:14 -0500

    createList y correcciones

--- Proyecto/html/HTMLGenerator.java
+++ Proyecto/html/HTMLGenerator.java
@@ -1,21 +1,44 @@
 package html;
 public class HTMLGenerator {
 	public static void main(String[]args) {
-		System.out.print(createHeader("Pato"));
+		String[]lista=new String[3];
+		String html="";
+		lista[0]="Pato";
+		lista[1]="Perro";
+		lista[2]="Gato";
+		
+		html+=createHeader("Pato");
+		html+=createList(lista,true);
+		html+=createParagraph("ANRJASDHAJFJKAFHjhfajdhfjadfkjadf");
+		html+=createLink("https://www.google.com/","Google");
+		html+=createTitle("Pagina");
+		System.out.print(html);
 	}
 	public static String createTitle(String title) {
-		String html="<head>\n<title>"+title+"</title>\n</head>";
+		String html="<head>\n<title>"+title+"</title>\n</head>\n";
 		return html;
 	}
 	public static String createHeader(String header) {
-		String html="<head>\n<header>"+header+"</header>\n</head>";
+		String html="<head>\n<header>"+header+"</header>\n</head>\n";
 		return html;
 	}
 	public static String createParagraph(String text) {
-		String html="<body>\n<p>"+text+"</p>\n</body>";
+		String html="<body>\n<p>"+text+"</p>\n</body>\n";
 		return html;
 	}
 	public static String createLink(String url, String linkText) {
-       	        return "<a href=\"" + url + "\">" + linkText + "</a>\n";
+       	 return "<body>\n<a href=\"" + url + "\">" + linkText + "</a>\n</body>\n";
     }
+	public static String createList(String[] lista, boolean ordenada){
+		String html="<body>\n";
+		if(ordenada==true)  html+="<ol>";
+		else html+="<ul>";
+		for(int i=0;i<lista.length;i++) {
+			html+="\n<li>"+lista[i]+"</li>";
+		}
+		if(ordenada==true)  html+="\n</ol>";
+		else html+="\n</ul>";
+		html+="\n</body>\n";
+		return html;
+	}
 }
